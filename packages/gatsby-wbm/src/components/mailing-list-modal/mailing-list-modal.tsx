60f3719473b23645e1ffc83456169d1a7e6e268e
Sanmati Kumar Jain <i.sanmati.jain@redcarpetup.com>
2020-10-08 16:05:36 +0530

    fix ml_pop_up mobile issue

--- packages/gatsby-wbm/src/components/mailing-list-modal/mailing-list-modal.tsx
+++ packages/gatsby-wbm/src/components/mailing-list-modal/mailing-list-modal.tsx
@@ -1,24 +1,26 @@
-import React, { useEffect } from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import React, { useEffect, useState } from 'react';
+import { makeStyles, createStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 
 import Newsletter from '../newsletter/newsletter';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    modal: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  })
-);
-
 export default function MailingListModal(props: any) {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+  const useStyles = makeStyles(() =>
+    createStyles({
+      modal: {
+        display: matches ? 'flex' : 'block',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    })
+  );
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if ('action' in props.query && props.query.action === 'ml_pop_up') {
