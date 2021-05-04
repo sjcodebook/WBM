d68e71243193412e1952329ab47666b223a396f5
Sanmati Kumar Jain <i.sanmati.jain@redcarpetup.com>
2021-05-05 01:58:34 +0530

    add firebase func

--- packages/gatsby-wbm/firebase/functions/index.js
+++ packages/gatsby-wbm/firebase/functions/index.js
@@ -1,11 +1,73 @@
-const cors = require('cors')({ origin: true });
+const Twitter = require('twitter-lite');
+const axios = require('axios');
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    // your function body here - use the provided req and res from cors
-    console.log(request.body);
-    // functions.logger.info('Hello logs!', { structuredData: true });
-    response.status(200).send({ message: 'msg' });
+exports.tweet2pdf = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    if (!request.body.tweetId) {
+      return response
+        .status(500)
+        .send({ message: 'tweet2pdf. tweetId not sent in request' });
+    }
+    const tweetId = request.body.tweetId;
+    const client = new Twitter({
+      consumer_key: 'WL1aTfplwrgyb8QiqHEFZISBY',
+      consumer_secret: 'Bng63BzIGTlg7KW9lgIvvy2s7kyVOoeGv9zjZo9VjIlwx47lZH',
+      access_token_key: '324284992-yZEodFeZdxApW8k7OdnLtuRHt6tWcdLIfoVgYOnB',
+      access_token_secret: 'Cjg29YRRDjT8AuOPtyJCeU6yp8YXK2XrQ2pdPVPJE2S60',
+    });
+
+    // const tweetData = await client
+    //   .get('statuses/show', {
+    //     id: tweetId,
+    //     include_entities: true,
+    //     tweet_mode: 'extended',
+    //     trim_user: true,
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+    let jiii = new Date(2019, 11, 24, 10, 33, 30, 0);
+    let loda = jiii.toISOString();
+    const tweetData = await axios
+      .get('https://api.twitter.com/2/tweets/search/recent', {
+        headers: {
+          Authorization: `Bearer AAAAAAAAAAAAAAAAAAAAAO5ELAEAAAAAwBeyoXnzEqlUgsO0LJoDZZVZC84%3D3sKcVIEZQNiHTcV3WAuYjNVINefA9uqIHyCo3O6UlyEm4jxmWQ`,
+        },
+        params: {
+          query: 'conversation_id:1327688091642028033',
+          // // expansions: 'referenced_tweets.id.author_id,author_id',
+          // // 'user.fields': 'name,username',
+          // // 'tweet.fields': 'author_id',
+          max_results: 90,
+          // q:
+          //   'from:oliverjumpertz to:oliverjumpertz',
+          // since_id: '1327688091642028033',
+          // count: 100,
+        },
+      })
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+        return response.data;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log('===================================');
+        console.log(error);
+      });
+
+    // const tweetData = await client
+    //   .get('2/tweets/1327688091642028033', {
+    //     expansions: 'referenced_tweets.id,referenced_tweets.id.author_id',
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
+    // console.log(tweetData);
+
+    response.status(200).send({ tweetData });
   });
 });
--- packages/gatsby-wbm/src/pages/tweet2pdf.tsx
+++ packages/gatsby-wbm/src/pages/tweet2pdf.tsx
@@ -10,6 +10,7 @@ type Tweet2pdfPageProps = {};
 
 const tweet2pdf: React.FunctionComponent<Tweet2pdfPageProps> = (props: any) => {
   const [tweetId, settweetId]: any = useState(null);
+  const [tweetData, settweetData]: any = useState(null);
 
   useEffect(() => {
     const query = queryString.parse(props.location.search);
@@ -19,13 +20,14 @@ const tweet2pdf: React.FunctionComponent<Tweet2pdfPageProps> = (props: any) => {
         settweetId(parsedTweetId.id);
         axios
           .post(
-            'http://localhost:5001/twitter-thread-pdf/us-central1/helloWorld',
+            'http://localhost:5001/twitter-thread-pdf/us-central1/tweet2pdf',
             {
               tweetId: parsedTweetId.id,
             }
           )
           .then(function (response) {
             console.log(response);
+            settweetData(response.data);
           })
           .catch(function (error) {
             console.log(error);
@@ -34,6 +36,34 @@ const tweet2pdf: React.FunctionComponent<Tweet2pdfPageProps> = (props: any) => {
     }
   }, [props.location.search]);
 
+  const syntaxHighlight = (json: any) => {
+    if (typeof json != 'string') {
+      json = JSON.stringify(json, undefined, 2);
+    }
+    json = json
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+    return json.replace(
+      /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+      function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+          if (/:$/.test(match)) {
+            cls = 'key';
+          } else {
+            cls = 'string';
+          }
+        } else if (/true|false/.test(match)) {
+          cls = 'boolean';
+        } else if (/null/.test(match)) {
+          cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+      }
+    );
+  };
+
   return (
     <Layout>
       <SEO
@@ -41,6 +71,12 @@ const tweet2pdf: React.FunctionComponent<Tweet2pdfPageProps> = (props: any) => {
         description="tweet2pdf by WebBrainsMedia || Convert and download twitter tweets/threads in PDF"
       />
       {tweetId}
+      <br />
+      <br />
+      <div
+        dangerouslySetInnerHTML={{ __html: syntaxHighlight(tweetData) }}
+      ></div>
+      {}
     </Layout>
   );
 };
